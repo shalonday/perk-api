0ef5c49bc7d497b0358a192fae9e7acbcdb0324e
Salvador Pio Alonday <salvadorpio.alonday@hcl-software.com>
2025-05-16 18:49:26 +0800

    Resolve 'X-Forwarded-For' header error

--- app.js
+++ app.js
@@ -41,6 +41,8 @@ app.post(
   neo4jService.createUser
 );
 
+app.set('trust proxy', 1);
+
 // Set up rate limiter: maximum of twenty requests per minute
 const limiter = RateLimit({
   windowMs: 1 * 60 * 1000,
